21a8a379abae1474c2c7b57cca00f48fe479339a
HiroMinase <lostsymbol612@gmail.com>
2023-08-27 22:05:01 +0900

    不要な記述の削除

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
